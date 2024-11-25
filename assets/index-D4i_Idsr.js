fa80392601feb72f2b8fc168e3a8e61fa1b1642e
yutsick <yutsick@users.noreply.github.com>
2024-11-25 23:09:32 +0000

    deploy: 974cb21e7243667682b63c8f0fe57ef8c9563a40

--- assets/index-D4i_Idsr.js
+++ assets/index-D4i_Idsr.js
@@ -111,7 +111,7 @@
         <div class="flex  mt-3">
           <!-- Logo -->
           <div class=" px-5 max-w-[24%] h-auto items-center flex">
-            <img src="/assets/images/${a}" alt="DreamCloud Logo" class="w-full">
+            <img src="./assets/images/${a}" alt="DreamCloud Logo" class="w-full">
           </div>
   
           <!-- List -->
